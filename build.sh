5c528084801fdc30d386227a64444fcdc48cd039
Alejandro Rodríguez Clemente <me@arcx.es>
2017-05-25 23:13:45 +0200

    Move the build script to the root path

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Version key/value should be on his own line
 
-PACKAGE_VERSION=$(cat ../package.json \
+PACKAGE_VERSION=$(cat package.json \
     | grep version \
     | head -1 \
     | awk -F: '{ print $2 }' \
     | sed 's/[",]//g' \
     | tr -d '[[:space:]]')
 
-PACKAGE_NAME=$(cat ../package.json \
+PACKAGE_NAME=$(cat package.json \
     | grep name \
     | head -1 \
     | awk -F: '{ print $2 }' \
