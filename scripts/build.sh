160a5370b854f0bd16a93bc186e9b63a66562de2
Alejandro Rodríguez Clemente <me@arcx.es>
2017-05-25 23:07:13 +0200

    Added build script

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Version key/value should be on his own line
+
+PACKAGE_VERSION=$(cat ../package.json \
+    | grep version \
+    | head -1 \
+    | awk -F: '{ print $2 }' \
+    | sed 's/[",]//g' \
+    | tr -d '[[:space:]]')
+
+PACKAGE_NAME=$(cat ../package.json \
+    | grep name \
+    | head -1 \
+    | awk -F: '{ print $2 }' \
+    | sed 's/[",]//g' \
+    | tr -d '[[:space:]]')
+
+docker build -t $PACKAGE_NAME:$PACKAGE_VERSION .
+
